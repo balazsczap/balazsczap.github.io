f36d02d34c0680820acd171e78101b49bbcd671a
Balázs Czap <balazsczap@gmail.com>
2021-11-27 19:06:15 +0100

    Cleanup repo

--- pages/np_teljes/index.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-	loadLinks();
-	sendIP();
-});
--- pages/rubik/js/lib/require-jquery.js
+++
@@ -1,4 +0,0 @@
-define(["js/lib/jquery-3.1.1.js"], function() {
-  // Raw jQuery does not return anything, so return it explicitly here.
-  return jQuery;
-})
